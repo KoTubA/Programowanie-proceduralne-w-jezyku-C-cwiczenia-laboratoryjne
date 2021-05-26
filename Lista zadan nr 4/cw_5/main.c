3bab6b67e745c30be2324665cb98cfd9e3d3abce
KoTubA <szymon.kotarba988@gmail.com>
2021-05-26 17:08:17 +0200

    Update solution

--- Lista zadan nr 4/cw_5/main.c
+++ Lista zadan nr 4/cw_5/main.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+typedef struct Node
+{
     int data;
-    struct node *left, *right;
+    struct Node * left, *right;
 }Node;
 
-Node *p = NULL;
-
-void PrintTree(Node *wsk_str) {
-    if(wsk_str == NULL) return;
-    if(wsk_str->left!=NULL) PrintTree(wsk_str->left);
-    printf("%d\n",wsk_str->data);
-    if(wsk_str->right!=NULL) PrintTree(wsk_str->right);
+void printNode(Node * p){
+    if(p == NULL){
+        return;
+    }
+    printNode(p->left);
+    printf("%d\n",p->data);
+    printNode(p->right);
 }
 
 int main()
 {
+    Node * head = (Node *) malloc(sizeof(Node));
+    head->data = 9;
+    head->left = (Node *) malloc(sizeof(Node));
+    head->right = (Node *) malloc(sizeof(Node));
+    Node * p = head->left;
+    Node * q = head->right;
+    p->data = 6;
+    p->left = (Node *) malloc(sizeof(Node));
+    p->right = (Node *) malloc(sizeof(Node));
+
+    p->left->data = 4;
+    p->left->left = (Node *) malloc(sizeof(Node));
+    p->left->right = NULL;
+    p->left->left->data = 2;
+    p->left->left->left = NULL;
+    p->left->left->right = NULL;
+
+    p->right->data = 7;
+    p->right->left = NULL;
+    p->right->right = NULL;
+
+
+    q->data = 11;
+    q->left = NULL;
+    q->right = (Node *) malloc(sizeof(Node));
+
+    q->right->data = 15;
+    q->right->left = NULL;
+    q->right->right = NULL;
+
+    Node * esa = head;
+    printNode(esa);
+
     return 0;
 }
